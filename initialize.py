69fb0bb2e930fc1902215d0a1416b609618cc2c6
John Eo <john.eo@rackspace.com>
2012-05-18 16:44:36 -0500

    Data population

--- initialize.py
+++ initialize.py
@@ -21,11 +21,31 @@ db.session.commit()
 
 # Populate shuttle_leg table
 
-time0715 = 7 * 60 + 15 # Minutes since midnight
+# Minutes since midnight
+time0715 = 7 * 60 + 15
+time0730 = 7 * 60 + 30 
+time0745 = 7 * 60 + 45
+time0800 = 8 * 60
 
-to_san_0715 = ShuttleLeg(aus, castle, time0715, None, 0, shuttle_big)
-db.session.add(to_san_0715)
-db.session.commit()
+time1600 = 16 * 60
+time1715 = 17 * 60 + 15
+
+for d in range(5):
+    from_aus_0715 = ShuttleLeg(aus, castle, time0715, None, d, shuttle_big)
+    from_spm_0745 = ShuttleLeg(spm, castle, time0745, None, d, shuttle_big)
+    from_cas_1600 = ShuttleLeg(castle, aus, time1600, None, d, shuttle_big)
+    db.session.add(from_aus_0715)
+    db.session.add(from_spm_0745)
+    db.session.add(from_cas_1600)
+    db.session.commit()
+
+for d in range(1, 4):
+    from_aus_0730 = ShuttleLeg(aus, castle, time0730, None, d, shuttle_small)
+    from_spm_0800 = ShuttleLeg(spm, castle, time0800, None, d, shuttle_small)
+    from_cas_1715 = ShuttleLeg(castle, aus, time1715, None, d, shuttle_small)
+    db.session.add(from_aus_0730)
+    db.session.add(from_cas_1715)
+    db.session.commit()
 
 """
 Do the following to reconstitute datetime object based on minutes since midnight
